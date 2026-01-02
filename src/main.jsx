420c210e9fbf4b4ba171b483881b23104ce90e93
azeemazad95 <azeemazad95@gmail.com>
2026-01-02 15:50:30 +0530

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 // Using HashRouter for GitHub Pages/static hosting compatibility (no server-side routing)
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
+
 
 createRoot(document.getElementById('root')).render(
 
-    <HashRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
 
 )
